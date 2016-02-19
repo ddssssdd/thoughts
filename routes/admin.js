132bbdfa25c4c8af943ca303b0b5c26ec89a5629
Steven Fu <sfu@synvata.com>
2016-02-19 10:11:58 +0800

    add stderr to run_command

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,34 @@
 var express = require("express");
 var router = express.Router();
 
-function RunCmd(cmd, args, cb, end) {
+function RunCmd(cmd, args, cb, end,err) {
     var spawn = require('child_process').spawn,
         child = spawn(cmd, args),
         me = this;
-    child.stdout.on('data', function (buffer) { cb(me, buffer) });
+    child.stdout.on('data', function (buffer) {
+    	cb(me, buffer) 
+ 	});
+ 	child.stderr.on("data",function(buffer){
+ 		if (err) {
+ 			err(me,buffer);
+ 		}else{
+ 			cb(me,buffer);
+ 		}
+
+ 	});
     child.stdout.on('end', end);
 }
 router.get("/backup",function(req,res){
  	var backup =new RunCmd(
  		'mongodump',//mongodump mongodump  -h 127.0.0.1 -d thoughts --archive=db_backup.zip --gzip 
- 		['-h','127.0.0.1','-d', 'thoughts', '--gzip', '--excludeCollectionsWithPrefix=book','--archive=db_backup.zip'],
+ 		['-h','127.0.0.1','-d', 'thoughts', '--gzip', 
+ 		'--excludeCollectionsWithPrefix=book',
+ 		'--archive=db_backup.zip',
+ 		'--excludeCollection=user_logs',
+ 		'--excludeCollection=mySessions',
+ 		'--excludeCollection=user_search_histories',],
  		function(me,buffer){ me.stdout += (buffer || 'nothing').toString()},
- 		function(){
- 			console.log(backup.stdout);
+ 		function(){ 			
  			if (backup.stdout){
  				res.json(backup.stdout.split("\n"));	
  			}else{
@@ -25,12 +39,14 @@ router.get("/backup",function(req,res){
  	)
 });
 router.get("/restore",function(req,res){
+	debugger;
  	var backup =new RunCmd(
  		'mongorestore',//'mongodump',
  		['-h','127.0.0.1','-d', 'thoughts', '--gzip', '--archive=db_backup.zip','--drop'],
- 		function(me,buffer){ me.stdout +=(buffer || 'nothing').toString()},
- 		function(){
- 			console.log(backup.stdout);
+ 		function(me,buffer){ 
+ 			me.stdout +=(buffer || 'nothing').toString()
+ 		},
+ 		function(){ 			
  			if (backup.stdout){
  				res.json(backup.stdout.split("\n"));	
  			}else{
